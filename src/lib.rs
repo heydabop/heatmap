71b17380370dd056113b15ba504b4ce891fe6d2d
heydabop <heydabop@gmail.com>
2019-11-12 00:48:01 -0600

    haversine

--- src/lib.rs
+++ src/lib.rs
@@ -105,3 +105,36 @@ pub fn get_pts(gpx: &str) -> Vec<TrkPt> {
 
     trk_pts
 }
+
+pub fn haversine(lat1: f64, lng1: f64, lat2: f64, lng2: f64) -> f64 {
+    let r = 6371e3; // earth mean radius in meters
+
+    let lat_rad_1 = lat1.to_radians();
+    let lat_rad_2 = lat2.to_radians();
+    let lat_delta = (lat2 - lat1).to_radians();
+    let lng_delta = (lng2 - lng1).to_radians();
+
+    let lat_sin = (lat_delta / 2.0).sin();
+    let lng_sin = (lng_delta / 2.0).sin();
+
+    let a = lat_sin.mul_add(
+        lat_sin,
+        lat_rad_1.cos() * lat_rad_2.cos() * lng_sin * lng_sin,
+    );
+    let c = 2.0 * a.sqrt().atan2((1.0 - a).sqrt());
+
+    r * c
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn haversine_test() {
+        assert!(
+            (haversine(31.2626, -100.3555, 38.1345, -89.6150) - 1_242_682.405_520_137_2).abs()
+                < std::f64::EPSILON
+        );
+    }
+}
