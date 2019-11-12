bbcca464f2b857621189e595e7d6b7566b7db1b8
heydabop <heydabop@gmail.com>
2019-11-12 01:29:17 -0600

    get mapbox zoom level

--- src/main.rs
+++ src/main.rs
@@ -32,4 +32,19 @@ fn main() {
     }
 
     println!("{}, {}\n{}, {}", max_lat, max_lng, min_lat, min_lng);
+
+    let mid_lat = min_lat + (max_lat - min_lat) / 2.0;
+    let map_width_meters = heatmap::haversine(mid_lat, min_lng, mid_lat, max_lng);
+    println!("width: {}", map_width_meters);
+    let map_width_pixels = 800.0;
+    let meters_per_pixel = map_width_meters / map_width_pixels;
+    println!("meters per pixel: {}", meters_per_pixel);
+
+    let zoom_level = ((10_018_755.0 * mid_lat.to_radians().cos()) / meters_per_pixel).ln()
+        / std::f64::consts::LN_2
+        - 7.0;
+
+    let mid_lng = min_lng + (max_lng - min_lng) / 2.0;
+
+    println!("{}, {} zoom: {}", mid_lat, mid_lng, zoom_level);
 }
