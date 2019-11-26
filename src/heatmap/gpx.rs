449d2e5d79ea9e983800d96441421fd42ef458de
heydabop <heydabop@gmail.com>
2019-11-26 08:03:58 -0600

    give each tag its own function in gpx

--- src/heatmap/gpx.rs
+++ src/heatmap/gpx.rs
@@ -17,9 +17,6 @@ pub fn get_pts(
         None => None,
     };
 
-    let mut in_trk = false; // true if we're between a <trk> and </trk> tag (the bulk of the gpx file)
-    let mut in_trkseg = false; // true if we're between a <trkseg> and </trkseg> tag
-
     let mut trk_pts = Vec::new();
 
     loop {
@@ -27,7 +24,7 @@ pub fn get_pts(
             Ok(Event::Start(ref e)) => match e.name() {
                 b"metadata" => {
                     if start.is_some() || end.is_some() {
-                        if let Some(ref time) = metadata_time(&mut reader, &mut buf)? {
+                        if let Some(ref time) = parse_metadata(&mut reader, &mut buf)? {
                             if let Some(start) = start {
                                 if time < start {
                                     return Ok(Vec::new());
@@ -41,35 +38,7 @@ pub fn get_pts(
                         }
                     }
                 }
-                b"trk" => in_trk = true, // mark that we're within <trk> </trk>, which we will be for most of the file
-                b"trkseg" => in_trkseg = true, // mark that we're within <trkseg> </trkseg>
-                b"type" => {
-                    if in_trk
-                        && filter_string.is_some()
-                        && !type_check(&mut reader, &mut buf, filter_string.unwrap())?
-                    {
-                        return Ok(Vec::new());
-                    }
-                }
-                b"trkpt" => {
-                    if !in_trk {
-                        // we could ignore a <trkpt> outside of <trk> but this seems malformed so we error out
-                        bail!("trkpt out of trk");
-                    }
-                    if !in_trkseg {
-                        // we could ignore a <trkpt> outside of <trkseg> but this seems malformed so we error out
-                        bail!("trkpt out of trkseg");
-                    }
-
-                    if let Some(trkpt) = trkpt(&mut reader, e)? {
-                        trk_pts.push(trkpt);
-                    }
-                }
-                _ => (),
-            },
-            Ok(Event::End(ref e)) => match e.name() {
-                b"trk" => in_trk = false,
-                b"trkseg" => in_trkseg = false,
+                b"trk" => trk_pts = parse_trk(&mut reader, &mut buf, filter_string)?,
                 _ => (),
             },
             Ok(Event::Eof) => break,
@@ -83,11 +52,10 @@ pub fn get_pts(
     Ok(trk_pts)
 }
 
-fn metadata_time(
-    reader: &mut Reader<&[u8]>,
-    buf: &mut Vec<u8>,
+fn parse_metadata(
+    mut reader: &mut Reader<&[u8]>,
+    mut buf: &mut Vec<u8>,
 ) -> Result<Option<DateTime<Utc>>, SimpleError> {
-    let mut in_time = false; // true if we're in a <time> tag (the next event should be the Text of the tag))
     let mut time = None;
 
     loop {
@@ -96,39 +64,26 @@ fn metadata_time(
         match reader.read_event(buf) {
             Ok(Event::Start(ref e)) => {
                 if let b"time" = e.name() {
-                    in_time = true; // mark that we're in a <time> tag and the next Text event is the start time of the gpx
+                    time = parse_time(&mut reader, &mut buf)?;
                 }
             }
-            Ok(Event::End(ref e)) => match e.name() {
-                b"metadata" => return Ok(time),
-                b"time" => in_time = false,
-                _ => (),
-            },
-            Ok(Event::Text(e)) => {
-                if in_time {
-                    // if we're in <time> read and parse it
-                    time = Some(
-                        e.unescape_and_decode(&reader)
-                            .unwrap()
-                            .parse::<DateTime<Utc>>()
-                            .expect("Error parsing timestamp from time"),
-                    );
+            Ok(Event::End(ref e)) => {
+                if let b"metadata" = e.name() {
+                    return Ok(time);
                 }
             }
-            Ok(Event::Eof) => bail!("Hit EOF while getting time from <metadata>"),
+            Ok(Event::Eof) => bail!("Hit EOF while in <metadata>"),
             Err(e) => bail!("Error at position {}: {:?}", reader.buffer_position(), e),
             _ => (),
         }
     }
 }
 
-fn trkpt(
-    reader: &mut Reader<&[u8]>,
+fn parse_trkpt(
+    mut reader: &mut Reader<&[u8]>,
     event: &BytesStart,
 ) -> Result<Option<super::TrkPt>, SimpleError> {
     let mut buf = Vec::new();
-
-    let mut in_time = false; // true if we're in a <time> tag (the next event should be the Text of the tag))
 
     let mut lat: Option<f64> = None;
     let mut lng: Option<f64> = None;
@@ -169,12 +124,11 @@ fn trkpt(
         match reader.read_event(&mut buf) {
             Ok(Event::Start(ref e)) => {
                 if let b"time" = e.name() {
-                    in_time = true; // mark that we're in a <time> tag and the next Text event is time for our trk_pt
+                    time = parse_time(&mut reader, &mut buf)?;
                 }
             }
-            Ok(Event::End(ref e)) => match e.name() {
-                b"time" => in_time = false,
-                b"trkpt" => {
+            Ok(Event::End(ref e)) => {
+                if let b"trkpt" = e.name() {
                     if lat.is_none() || lng.is_none() {
                         eprintln!("Incomplete <Trackpoint>: {:?} {:?} {:?}", lat, lng, time);
                         return Ok(None);
@@ -187,25 +141,107 @@ fn trkpt(
                         time,
                     }));
                 }
-                _ => (),
-            },
-            Ok(Event::Text(e)) => {
-                if in_time {
-                    // if we're in <time> read and parse it for trk_pt
-                    time = Some(
-                        e.unescape_and_decode(&reader)
-                            .unwrap()
-                            .parse::<DateTime<Utc>>()
-                            .expect("Error parsing timestamp from time"),
-                    );
-                }
             }
-            Ok(Event::Eof) => bail!("Hit EOF while getting data from <trkpt>"),
+            Ok(Event::Eof) => bail!("Hit EOF while in <trkpt>"),
             Err(e) => bail!("Error at position {}: {:?}", reader.buffer_position(), e),
             _ => (),
         }
 
         buf.clear();
+    }
+}
+
+fn parse_trk(
+    mut reader: &mut Reader<&[u8]>,
+    mut buf: &mut Vec<u8>,
+    filter_string: Option<&str>,
+) -> Result<Vec<super::TrkPt>, SimpleError> {
+    let mut trk_pts = Vec::new();
+
+    loop {
+        buf.clear();
+
+        match reader.read_event(buf) {
+            Ok(Event::Start(ref e)) => match e.name() {
+                b"trkseg" => trk_pts = parse_trkseg(&mut reader, &mut buf)?,
+                b"type" => {
+                    if filter_string.is_some()
+                        && !type_check(&mut reader, &mut buf, filter_string.unwrap())?
+                    {
+                        return Ok(Vec::new());
+                    }
+                }
+                _ => (),
+            },
+            Ok(Event::End(ref e)) => {
+                if let b"trk" = e.name() {
+                    return Ok(trk_pts);
+                }
+            }
+            Ok(Event::Eof) => bail!("Hit EOF while in <trk>"),
+            Err(e) => bail!("Error at position {}: {:?}", reader.buffer_position(), e),
+            _ => (),
+        }
+    }
+}
+
+fn parse_trkseg(
+    mut reader: &mut Reader<&[u8]>,
+    buf: &mut Vec<u8>,
+) -> Result<Vec<super::TrkPt>, SimpleError> {
+    let mut trk_pts = Vec::new();
+
+    loop {
+        buf.clear();
+
+        match reader.read_event(buf) {
+            Ok(Event::Start(ref e)) => {
+                if let b"trkpt" = e.name() {
+                    if let Some(trkpt) = parse_trkpt(&mut reader, e)? {
+                        trk_pts.push(trkpt);
+                    }
+                }
+            }
+            Ok(Event::End(ref e)) => {
+                if let b"trkseg" = e.name() {
+                    return Ok(trk_pts);
+                }
+            }
+            Ok(Event::Eof) => bail!("Hit EOF while in <trkseg>"),
+            Err(e) => bail!("Error at position {}: {:?}", reader.buffer_position(), e),
+            _ => (),
+        }
+    }
+}
+
+fn parse_time(
+    reader: &mut Reader<&[u8]>,
+    buf: &mut Vec<u8>,
+) -> Result<Option<DateTime<Utc>>, SimpleError> {
+    let mut time = None;
+
+    loop {
+        buf.clear();
+
+        match reader.read_event(buf) {
+            Ok(Event::Text(e)) => {
+                // read and parse text value in <time>
+                time = Some(
+                    e.unescape_and_decode(&reader)
+                        .unwrap()
+                        .parse::<DateTime<Utc>>()
+                        .expect("Error parsing timestamp from time"),
+                );
+            }
+            Ok(Event::End(ref e)) => {
+                if let b"time" = e.name() {
+                    return Ok(time);
+                }
+            }
+            Ok(Event::Eof) => bail!("Hit EOF while in <time>"),
+            Err(e) => bail!("Error at position {}: {:?}", reader.buffer_position(), e),
+            _ => (),
+        }
     }
 }
 
